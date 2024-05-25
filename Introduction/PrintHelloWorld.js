79fef16222f1653140d1ee27340ab967715589e0
Jv2350 <tp8375735@gmail.com>
2024-05-25 22:40:42 +0530

    Hello World program

---
+++ Introduction/PrintHelloWorld.js
@@ -0,0 +1,7 @@
+// hello world program
+console.log("Hello world"); // mostly used when there is need of debugging
+
+alert("Hello World") // pop-up
+
+document.write("Hello Worldddd") // used when we want to display on the webpage
+
